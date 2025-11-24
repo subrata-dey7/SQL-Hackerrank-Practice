0e04f8b93b12d5e89db62b2c2fb51805d0868af9
Subrata Dey <dey7subrata@gmail.com>
2025-11-24 22:55:42 +0530

    Add files via upload

---
+++ 04-Select_by_ID/04-Select_by_ID.sql
@@ -0,0 +1,9 @@
+
+# 4) Select by ID
+
+-- Select all columns from the table
+SELECT *
+-- Specify the table to retrieve data from
+FROM CITY
+-- Filter the results to only the row where ID is 1661
+WHERE ID = 1661;
